698c0fa27a1d320a785eba8285a2741067220b58
Sam Fitzsimmons <samfitzsimmons21@gmail.com>
2024-12-02 13:39:22 +0000

    2024 Day 2 Tasks Completed

--- 2024/Day2/Day2Task.py
+++ 2024/Day2/Day2Task.py
@@ -1,35 +1,37 @@
 with open("input.txt") as f:
     data = [list(map(int, line.split())) for line in f.readlines()]
 
-safe_amount = 0
-
 def is_safe(sequence):
     inc_flag = False
     dec_flag = False
     n = len(sequence)
     for i in range(1, n):
         diff = abs(sequence[i] - sequence[i - 1])
-        if diff < 1 or diff > 3:  
+        if diff not in range(1, 4):  
             return False
+        
         if sequence[i] > sequence[i - 1]:
             inc_flag = True
+
         if sequence[i] < sequence[i - 1]:
             dec_flag = True
+
         if inc_flag and dec_flag: 
             return False
     return True
 
-for line in data:
+safe_amount = 0
+for record in data:
 
-    if is_safe(line):
+    if is_safe(record):
         safe_amount += 1
         continue
 
-    n = len(line)
+    n = len(record)
     for i in range(n):
 
-        modified_line = line[:i] + line[i + 1:]
-        if is_safe(modified_line):
+        parsed_record = record[:i] + record[i + 1:]
+        if is_safe(parsed_record):
             safe_amount += 1
             break  
 
