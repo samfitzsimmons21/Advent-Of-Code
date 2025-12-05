7489b0811492c11e290f08734997b0e2aa40ffd0
fitzsims <samfitzsimmons21@gmail.com>
2025-12-05 23:54:21 +0000

    Day 5 Part 2 Completed

--- 2025/Day5/day5.py
+++ 2025/Day5/day5.py
@@ -23,3 +23,34 @@ for line in second.splitlines():
 
 print("Part 1 Answer: ", p1_ans)
 
+# Part 2
+n = len(range_vals)
+
+changes = 1
+while changes > 0:
+    changes = 0
+
+    n = len(range_vals)
+    new_ranges = []
+
+    for start, end in range_vals:
+        if not new_ranges:
+            new_ranges.append([start, end])
+        else:
+            last_start, last_end = new_ranges[-1]
+
+            if start <= last_end:
+                new_ranges[-1][-1] = max(last_end, end)
+            else:
+                new_ranges.append([start, end])
+
+    range_vals = new_ranges
+    changes = len(new_ranges) - n
+
+p2_ans = 0
+for val in range_vals:
+    lower_range, upper_range = val
+    p2_ans += upper_range - lower_range + 1
+
+print("Part 2 Answer: ", p2_ans)
+
